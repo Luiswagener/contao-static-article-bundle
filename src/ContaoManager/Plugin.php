d370e0c3e40185234a50e957a4a73d8b827b70c3
luiswagener <info@luiswagener.de>
2025-01-27 22:31:33 +0100

    initial commit

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Luiswagener\Contao\StaticArticleBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Luiswagener\Contao\StaticArticleBundle\LuiswagenerStaticArticleBundle;
+
+class Plugin implements BundlePluginInterface
+{
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(LuiswagenerStaticArticleBundle::class)
+                ->setLoadAfter([
+                    ContaoCoreBundle::class
+                ]),
+        ];
+    }
+}
---
+++ src/LuiswagenerStaticArticleBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Luiswagener\Contao\StaticArticleBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LuiswagenerStaticArticleBundle extends Bundle
+{
+
+}
